fc65381e216e3830e7b51e5bac227d0ad1db75f4
Taha Ansari <taha.m.ansari@gmail.com>
2024-07-29 18:47:02 +0530

    wip

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import style from "../style.module.css";
 
-const page = ({searchParams}) => {
-    console.log(searchParams)
-    const params = searchParams;
-  return (
-    <div>This is blog inner page with id {params.id}</div>
-  )
+async function fetchPost(id) {
+  const fetchData = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await fetchData.json();
+  return data;
 }
 
-export default page
+const page = async ({ params }) => {
+  const { id } = params;
+  const getPost = await fetchPost(id);
+  console.log(getPost);
+  return (
+    <>
+      <div className={style.item}>
+        <Link href="/blogs">back</Link>
+        <div>Id : {getPost.id}</div>
+        <div>Title : {getPost.title}</div>
+        <div>Desc : {getPost.body}</div>
+      </div>
+    </>
+  );
+};
+
+export default page;
--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from "react";
 import style from "./style.module.css";
 
 // import Button from '../components/Button';
-import Link from 'next/link';
+import Link from "next/link";
 
-export async function fetchPosts(){
-    const posts = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
-    const data = posts.json();
-    return data;
+export async function fetchPosts() {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+  const data = posts.json();
+  return data;
 }
 
 const page = async () => {
   const posts = await fetchPosts();
 
-
   return (
     <>
-        {posts.map((item)=>{
-            return <div className={style.item} key={item.id}>
-                {item.title}
-                <Link href={`/blogs/${item.id}`} style={{color:"white"}}><button>View Blog</button></Link>
-            </div>
-        })}
+      {posts.map((item) => {
+        return (
+          <div className={style.item} key={item.id}>
+            <div>Id: {item.id}</div>
+            <div>Title: {item.title}</div>
+            <Link href={`/blogs/${item.id}`} style={{ color: "white" }}>
+              <button>View Blog</button>
+            </Link>
+          </div>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
